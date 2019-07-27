f852e39891f3ce34ab9affcb9405571d65beac0f
Philip Kirkbride <kirkins@gmail.com>
2019-07-27 16:08:24 -0400

    add script for grow item

---
+++ SlowJam_2019/Assets/Scripts/Entity/GrowItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowItem : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    public GameObject iceBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.CompareTag("Entity"))
+        {
+            // Call grow function below
+            //collider.gameObject.GetComponent<EntityController>().GrowFromItem();
+        }
+            
+        Destroy(gameObject);
+    }
+
+}
