6a7b63f0372279448ca1358a5260e7360c0a12ae
Matthew <MatthewFerreira514@gmail.com>
2019-07-27 17:51:42 -0400

    Made Force Zones modular into Zones

---
+++ SlowJam_2019/Assets/Scripts/Zones/WinZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinZone : Zone
+{
+    [Header("Win Zone Attributes")]
+    public List<Collider> detectedColliders = new List<Collider>();
+    public override void ManageZone()
+    {
+        Collider[] hitObjects = ReturnHitObjects(zoneSettings);
+
+        for (int i = 0; i < hitObjects.Length; i++)
+        {
+            if (!detectedColliders.Contains(hitObjects[i]))
+            {
+                detectedColliders.Add(hitObjects[i]);
+                
+                ActivateWinZone();
+            }
+        }
+    }
+
+    private void ActivateWinZone()
+    {
+        
+    }
+}
