5d978750545edef78abad86c8c4ad6afdcee0915
Philip Kirkbride <kirkins@gmail.com>
2019-07-27 18:19:16 -0400

    add class for getting score

---
+++ SlowJam_2019/Assets/Scripts/Entity/ScoreScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private int startScore;
+    private int endScore;
+    Collider m_Collider;
+
+    int GetScore(Vector3 v)
+    {
+        return (int)(v[0] * v[1] * v[2] * 1000);
+    }
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        InvokeRepeating("LevelDone", 1f, 1f);
+        startScore = GetScore(transform.localScale);
+    }
+
+    public void LevelDone()
+    {
+        endScore = GetScore(transform.localScale);
+    }
+}
